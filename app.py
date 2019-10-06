bd001f2354bae286ba0e82846c91fbf350330858
Guy Royse <guy@guyroyse.com>
2019-10-06 17:52:18 -0400

    load all data, two REST calls to fetch TopK shapes and words

--- app.py
+++ app.py
@@ -1,41 +1,32 @@
-import os
-os.environ['NLTK_DATA'] = './nltk_data'
-
-from flask import Flask, request
-
-from nltk.tokenize import sent_tokenize, word_tokenize
-from nltk.corpus import stopwords
-
-stopWords = set(stopwords.words('english'))
+from flask import Flask, jsonify
 
 from redisbloom.client import Client
 
-rb = Client()
-rb.topkReserve('ufo_words', 10, 10, 5, 0.9)
+client = Client()
 
 # the Flask app
 app = Flask(__name__, instance_relative_config=True)
 
-# hello world as a route
-@app.route('/hello')
-def hello():
-  return "👋🏻🌎"
+@app.route('/shapes')
+def shapes():
 
-@app.route('/words', methods=['POST'])
+  top_shapes = [{
+    'shape': shape,
+    'count': client.topkCount('ufo_shapes', shape)[0]
+  } for shape in client.topkList('ufo_shapes')]
+
+  return jsonify(top_shapes)
+
+@app.route('/words')
 def words():
-  data = request.form['sighting']
-  s = data.lower()
-  words = word_tokenize(s)
 
-  words_filtered = []
-  for w in words:
-    if w not in stopWords:
-      if w.isalpha():
-        rb.topkAdd('ufo_words', w)
+  top_words = [{ 
+    'word': word, 
+    'count': client.topkCount('ufo_words', word)[0]
+  } for word in client.topkList('ufo_words')]
 
-  top = rb.topkList('ufo_words')
-  print(top)
-  return "Done"
+  return jsonify(top_words)
+
 
 # kick off the Flask application
 if __name__ == '__main__':
--- build.py
+++ build.py
@@ -1,67 +1,69 @@
 import os
 os.environ['NLTK_DATA'] = './nltk_data'
 
+from redisbloom.client import Client
+
 import pandas as pd
 
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 
-from redisbloom.client import Client
-
-INPUT_FILE = 'nuforc_reports.csv'
 STOP_WORDS = set(stopwords.words('english'))
-SAMPLE_SIZE = 100
 
 def main():
+  client = setup_rebloom()
+  data = load_data('nuforc_reports.csv')
+  process_data(client, data)
 
+def setup_rebloom():
+  # create the client
+  client = Client()
+
+  # remove any old keys
+  client.delete('ufo_words', 'ufo_shapes')
+
+  # setup some Top-K action!
+  client.topkReserve('ufo_words', 10, 10, 5, 0.9)
+  client.topkReserve('ufo_shapes', 5, 4, 5, 0.9)
+
+  # return the client
+  return client
+
+def load_data(filename):
   # read in the CSV data
-  df = pd.read_csv(INPUT_FILE, encoding='utf-8')
-  print(f"Read {df.shape[0]} rows and {df.shape[1]} columns from '{INPUT_FILE}'.")
+  df = pd.read_csv(filename, encoding='utf-8')
+  print(f"Read {df.shape[0]} rows and {df.shape[1]} columns from '{filename}'.")
 
-  # pick some random ufo sightings
-  df = df.sample(n=SAMPLE_SIZE)
-  print(f"Selected {SAMPLE_SIZE} random rows.")
-
-  # drop all columns except 'summary', 'text', and 'shape'
-  df = df.filter(items=['summary', 'text', 'shape'])
+  # drop all columns except 'text' and 'shape'
+  df = df.filter(items=['text', 'shape'])
   print(f"Dropped all columns except {df.columns.to_list()}.")
 
   # make sure they're all strings
   df = df.astype(str)
 
-  # setup the client
-  rb = Client()
+  return df
 
-  # remove any old keys
-  rb.delete('ufo_words', 'ufo_shapes')
+def process_data(client, data):
+  for index, row in data.iterrows():
 
-  # setup some Top-K action!
-  rb.topkReserve('ufo_words', 10, 10, 5, 0.9)
-  rb.topkReserve('ufo_shapes', 5, 4, 5, 0.9)
-
-  # add the words and shapes
-  for index, row in df.iterrows():
-
+    # get the shape and the words
     shape = row['shape']
-    summary = parse_words(row['summary'])
-    text = parse_words(row['text'])
+    words = parse_words(row['text'])
 
-    print(f"Row: {index + 1} Shape: {shape} Summary: {len(summary)} Text: {len(text)}")
+    # print a status
+    print("Row   : ", index)
+    print("Shape : ", shape)
+    print("Words : ", words)
+    print()
 
-    rb.topkAdd('ufo_shapes', shape)
-    [rb.topkAdd('ufo_words', w) for w in summary]
-    [rb.topkAdd('ufo_words', w) for w in text]
-
-  # print the results
-  top_words = [(word, rb.topkCount('ufo_words', word)) for word in rb.topkList('ufo_words')]
-  top_shapes = [(shape, rb.topkCount('ufo_shapes', shape)) for shape in rb.topkList('ufo_shapes')]
-
-  print()
-  print("Top Words:", top_words)
-  print("Top Shapes :", top_shapes)
+    # add the shapes and words
+    client.topkAdd('ufo_shapes', shape)
+    [client.topkAdd('ufo_words', w) for w in words]
 
 def parse_words(text):
   words = word_tokenize(text.lower())
   return [w for w in words if w not in STOP_WORDS and w.isalpha()]
 
-main()
+
+if __name__ == '__main__':
+  main()
